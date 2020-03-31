2cda62cce59499e43855bffb9a1d75a350bb490b
Jiwclub <jakkraphan21544@gmail.com>
2020-03-31 19:41:25 +0700

    Component Data Sharing
    การสื่อสารระหว่าง component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Com1Component } from './components/com1/com1.component';
+import { SharedService } from './services/shared.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngCommunication';
+  version = 0;
+  isProduction=false;
+  account = {username: 'admin',password: '1234'};
+  time1 = 0;
+
+// เข้าถึงตัว com1
+@ViewChild("com1",{static: false}) com1:Com1Component
+  onClickViewChild() {
+    this.com1.count = 10
+  }
+  constructor(public shared:SharedService){
+
+  }
+
+  getResult() {
+    return 'Test'
+  }
+  onClick() {
+      this.version++;
+  }
+  onChange1(event){
+    this.time1 = event
+  }
+  onResetVersion() {
+    this.version = 0
+  }
 }
---
+++ src/app/services/shared.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+  private isLogged  = false;
+
+  constructor() { }
+  login() {
+    this.isLogged = true;
+  }
+  logout() {
+    this.isLogged = false;
+  }
+  getState() {
+    return this.isLogged;
+  }
+}
